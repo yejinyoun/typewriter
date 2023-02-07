161160ba7761605f7d98cfb205e8c4372e1bac9a
yejinyoun <yejinyoun@icloud.com>
2023-02-07 23:55:25 +0100

    loop done

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const written = document.querySelector(".typewritten").innerHTML;
+let i = 0;
+let letter;
+
+initWrite();
+
+function initWrite() {
+  document.querySelector(".typewritten").innerHTML = "";
+  write();
+}
+
+function write() {
+  /* read 1 letter */
+  letter = written.substring(i, i + 1);
+
+  /* print out 1 letter */
+  console.log(letter);
+  document.querySelector(".typewritten").innerHTML += letter;
+
+  /* mark each round of iteration */
+  i++;
+
+  /* check the condition and loop */
+  if (i < written.length) {
+    setTimeout(write, 500);
+  }
+}
